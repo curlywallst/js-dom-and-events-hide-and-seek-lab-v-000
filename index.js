e8ca86ef4e7202ca61e6527086d29459bc52b24b
curlywallst <nnoyes@comcast.net>
2017-12-07 12:50:53 -0500

    Done.

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const rankedLists = document.querySelectorAll('.ranked-list')
-  for (let i=0; i<rankedLists.length; i++) {
+  for (let i = 0; i < rankedLists.length; i++) {
     let rankedListsChildren = rankedLists[i].children
-    for (let l=0; l<rankedListsChildren.length; l++) {
-      rankedListsChildren[l].innerText = (parseInt(rankedListsChildren[l].innerText)+n).toString()
+    for (let l = 0; l < rankedListsChildren.length; l++) {
+      rankedListsChildren[l].innerHTML = parseInt(rankedListsChildren[l].innerHTML) + n
     }
   }
 }
